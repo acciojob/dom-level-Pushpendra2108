4a6f197ab509f98310bde47ff99818734927d0e4
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2025-01-01 09:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ const levelItem = document.querySelector('#level');
+
+        levelItem.addEventListener("click", () =>{
+            
+            alert("The level of the element is: 4") 
+        }
+      ); 
